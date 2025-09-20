c4e59e833879dc0a55ba1dd988b71ebdeded31aa
B.Sathvik Reddy <sathvikspartan@gmail.com>
2025-09-20 13:56:54 +0530

    Add files via upload

---
+++ Problem 1/Solution1.java
@@ -0,0 +1,21 @@
+/** Multiples of 3 or 5
+ *
+ *
+ */
+
+public class Solution1 {
+    public static void main(String[] args) {
+        int limit = 1000; // Since we need sum of multiples of 3 and 5.
+        int sum = 0;
+        for(int i = 1; i < limit; i++){
+            if(i % 3 == 0 || i % 5 == 0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
+/*
+Time complexity is O(n), where n is the limit.
+ */
