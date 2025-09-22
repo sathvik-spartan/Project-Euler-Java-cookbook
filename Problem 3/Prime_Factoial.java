d3665530b2340262273207ed28bbc8d538263004
B.Sathvik Reddy <sathvikspartan@gmail.com>
2025-09-22 13:21:58 +0530

    Create Prime_Factoial.java

---
+++ Problem 3/Prime_Factoial.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Prime_Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Prime factors are " + n);
+
+        // Print number of 2s that divide n
+        while (n % 2 == 0) {
+            System.out.print(2 + " ");
+            n /= 2;
+        }
+
+        // n must be odd at this point, so skip even numbers
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            while (n % i == 0) {
+                System.out.print(i + " ");
+                n /= i;
+            }
+        }
+
+        // If n is still a prime number > 2
+        if (n > 2) {
+            System.out.print(n);
+        }
+    }
+}
